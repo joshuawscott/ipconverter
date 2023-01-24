aec4f989ac47ac3b700e582961a57b31aa1d8965
Josh Scott <josh@simpli.fi>
2023-01-24 10:59:48 -0600

    change require

--- lib/ipconverter.rb
+++ lib/ipconverter.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'ipconverter/version'
-require 'ipconverter/ipconverter'
+require 'ipconverter'
 
 # Contains the methods for doing IP Address conversions
 #
